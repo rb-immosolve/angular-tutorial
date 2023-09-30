fbec8aaa8053715256582e97edcccd6b82fdd3ae
rahul_bhatnagar <rb@immosolve.de>
2023-09-30 22:40:08 +0200

    Using @ViewChild() instead of passing values

--- src/app/color-picker/color-options/color-options.component.ts
+++ src/app/color-picker/color-options/color-options.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-color-options',
@@ -8,6 +8,9 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class ColorOptionsComponent {
   @Output('colorChange') colorChange = new EventEmitter<string>()
   errorList:string = "";
+  @ViewChild('redValue') red:ElementRef;
+  @ViewChild('greenValue') green:ElementRef;
+  @ViewChild('blueValue') blue:ElementRef;
 
   ngOnInit() {}
 
@@ -15,25 +18,29 @@ export class ColorOptionsComponent {
     this.colorChange.emit(newColor);
   }
 
-  setAndChangeColor(red:string, green:string, blue:string){
+  setAndChangeColor(){
+    let red:number = parseInt(this.red.nativeElement.value);
+    let green:number = parseInt(this.green.nativeElement.value);
+    let blue:number = parseInt(this.blue.nativeElement.value);
+
     let errors:string[] = [];
-    if(isNaN(parseInt(red)) || parseInt(red).toString() != red){
+    if(isNaN(red)){
       errors.push('Red value must be an integer')
     }
-    if(isNaN(parseInt(green)) || parseInt(green).toString() != green){
+    if(isNaN(green)){
       errors.push('Green value must be an integer')
     }
-    if(isNaN(parseInt(blue)) || parseInt(blue).toString() != blue){
+    if(isNaN(blue)){
       errors.push('Blue value must be an integer')
     }
 
-    if(parseInt(red) < 0 || parseInt(red) > 255){
+    if(red < 0 || red > 255){
       errors.push('Red value must be between 0 and 255')
     }
-    if(parseInt(green) < 0 || parseInt(green) > 255){
+    if(green < 0 || green > 255){
       errors.push('Green value must be between 0 and 255')
     }
-    if(parseInt(blue) < 0 || parseInt(blue) > 255){
+    if(blue < 0 || blue > 255){
       errors.push('Blue value must be between 0 and 255')
     }
     if(errors.length > 0){
@@ -42,11 +49,10 @@ export class ColorOptionsComponent {
     }else{
       this.errorList = "";
     }
-    let redHex:string = parseInt(red).toString(16).padStart(2,'0');
-    let greenHex:string = parseInt(green).toString(16).padStart(2,'0');
-    let blueHex:string = parseInt(blue).toString(16).padStart(2,'0');
+    let redHex:string = red.toString(16).padStart(2,'0');
+    let greenHex:string = green.toString(16).padStart(2,'0');
+    let blueHex:string = blue.toString(16).padStart(2,'0');
     let finalColor:string = `#${redHex}${greenHex}${blueHex}`
     this.colorChange.emit(finalColor);
-
   }
 }
