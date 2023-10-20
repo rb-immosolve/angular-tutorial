29e803c732227d88b3aebb5e9b41f760b48547e7
Rahul Bhatnagar <ralbhat@gmail.com>
2023-10-20 02:55:26 +0200

    Must unsubscribe on destroy when you subscribe!

--- src/app/components/shopping/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping/shopping-list/shopping-list.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../../../model/ingredient.model';
 import { ShoppingListService } from 'src/app/lib/services/shopping-list.service';
 import { IngredientService } from 'src/app/lib/services/ingredient.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[] = [];
   errMsg: string = "";
+  errSubscription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService, private ingredientService: IngredientService) { }
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.shoppingList;
-    this.ingredientService.errSubject.subscribe((errMsg: string) => {
-      this.errMsg = errMsg
-    })
+    this.errSubscription = this.ingredientService.errSubject.subscribe((errMsg: string) => {
+      this.errMsg = errMsg;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.errSubscription.unsubscribe();
   }
 
 }
